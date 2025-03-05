e59d88b2e9e15dfc022ffa6675c5e263c7350a98
Matthew <matthewmonasterski@gmail.com>
2025-03-05 15:07:14 -0500

    pushing

--- template/Contact.java
+++ template/Contact.java
@@ -11,6 +11,17 @@ package template;
  */
 
 class Contact {
+	private String name;
+	private PhonebookEntry[] entries;
+	
+	public Contact (String name) {
+		this.name = name;
+		
+	}
+	
+	public void addPhonebookEntry (String name, String type) {
+		PhonebookEntry entry = new PhonebookEntry(name, type);
+	}
 	 
 
 }
